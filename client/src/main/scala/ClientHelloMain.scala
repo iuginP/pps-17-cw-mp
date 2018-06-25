33f5a19b1baf98942c206cd1b9a14eadef03a371
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2018-06-25 14:42:40 +0200

    Created dummy project with three subprojects (core, client and server)

---
+++ client/src/main/scala/ClientHelloMain.scala
@@ -0,0 +1,8 @@
+
+/**
+  * Hello class for client
+  */
+object ClientHelloMain extends App {
+  println(s"Core greeting from client: ${CoreExampleClass.greet}")
+  println("Hello i'm the client")
+}
