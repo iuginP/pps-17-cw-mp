5363a818e1f05cbb7e7ae4cebc4e3ca4b2ecfe81
Davide Borficchia <davide.borficchia@studio.unibo.it>
2018-07-06 15:42:46 +0200

    aggiunti controli input e corretto bug

--- client/src/main/scala/it/cwmp/controller/CreatePrivateRoomController.scala
+++ client/src/main/scala/it/cwmp/controller/CreatePrivateRoomController.scala
@@ -1,22 +1,58 @@
 package it.cwmp.controller
 
 import javafx.application.Platform
+import javafx.scene.control.{CheckBox, PasswordField, Spinner, TextField}
 import javafx.fxml.FXML
+import javafx.stage.Stage
 
 class CreatePrivateRoomController extends ViewController{
+
+  var stage: Stage = _
+
+  @FXML
+  private var roomName: TextField = _
+  @FXML
+  private var numPlayer: Spinner[Integer] = _
+  @FXML
+  private var roomPassword: PasswordField = _
+  @FXML
+  private var showPassword: CheckBox = _
 
 
   @FXML
   private def onClickCreate(): Unit = {
     Platform.runLater(() => {
-      //TODO CREATE ROOM
+      if (checkParam()){
+        //todo fare richiesta
+      }
+    })
+  }
+
+  @FXML
+  private def onChecked(): Unit = {
+    Platform.runLater(() => {
+
     })
   }
 
   @FXML
   private def onClickReset(): Unit = {
     Platform.runLater(() => {
-      //TODO CREATE ROOM
+      resetFields
     })
+  }
+
+  private def checkParam(): Boolean ={
+    if(roomName.getText() != "" || roomPassword.getText() != ""){
+      true
+    }
+    false
+  }
+
+  private def resetFields(): Unit = {
+    roomName setText("")
+    numPlayer getValueFactory() setValue 2
+    roomPassword setText("")
+    showPassword setSelected(false)
   }
 }
--- client/src/main/scala/it/cwmp/view/CreatePrivateRoomView.scala
+++ client/src/main/scala/it/cwmp/view/CreatePrivateRoomView.scala
@@ -1,28 +1,28 @@
 package it.cwmp.view
 
-import it.cwmp.controller.OpeningController
+import it.cwmp.controller.CreatePrivateRoomController
 import it.cwmp.utils.{LayoutRes, StringRes}
 import javafx.application.Platform
-import javafx.fxml.FXMLLoader
+import javafx.fxml.{FXMLLoader}
 import javafx.scene.Scene
 import javafx.scene.layout.Pane
 import javafx.stage.Stage
 
 class CreatePrivateRoomView extends View {
-  /**
-    * Main method that starts the view and show it to the user.
-    **/
+
+  var controller: CreatePrivateRoomController = _
+
   override def start(): Unit = {
     val mainStage = new Stage
-    mainStage setTitle StringRes.openingTitle
+    mainStage setTitle StringRes.CreatePrivateRoomTitle
     /*mainStage setHeight 400
     mainStage setWidth 600*/
     mainStage setResizable false
 
     val loader: FXMLLoader = new FXMLLoader(getClass.getResource(LayoutRes.createPrivateRoomLayout))
     val root: Pane = loader.load[Pane]
-//    controller = loader.getController[OpeningController]
-//    controller.stage = mainStage
+    controller = loader.getController[CreatePrivateRoomController]
+    controller.stage = mainStage
     val scene: Scene = new Scene(root)
 
     mainStage.setOnCloseRequest((_) => {
@@ -31,5 +31,6 @@ class CreatePrivateRoomView extends View {
     })
     mainStage setScene scene
     mainStage.show()
+
   }
 }
