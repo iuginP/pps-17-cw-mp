58e578868e03e239513e3e261cb4d8661237e7f0
Davide Borficchia <davide.borficchia@studio.unibo.it>
2018-07-10 16:32:54 +0200

    Creato l'attore per wrappare la gui

--- client/src/main/scala/it/cwmp/client/RoomManagerMain.scala
+++ client/src/main/scala/it/cwmp/client/RoomManagerMain.scala
@@ -1,16 +1,10 @@
 package it.cwmp.client
 
-import it.cwmp.client.view.RoomsManagerView
-import javafx.application.Platform
-import javafx.embed.swing.JFXPanel
+import akka.actor.{ActorSystem, Props}
+import it.cwmp.client.view.room.{RoomViewActor, RoomViewMessages}
 
 object RoomManagerMain extends App {
-
-  new JFXPanel
-  Platform setImplicitExit false
-  Platform runLater(() => {
-    val roomsManagerView = new RoomsManagerView
-    roomsManagerView.start()
-  })
-
+  val system = ActorSystem("test")
+  val actor = system.actorOf(Props[RoomViewActor], "roomView")
+  actor ! RoomViewMessages.ShowGUI
 }
