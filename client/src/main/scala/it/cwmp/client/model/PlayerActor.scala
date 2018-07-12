0fa3912a84792a03c26ff69987e42ec4c8edac80
iugin <eugenio.pierfederici@studio.unibo.it>
2018-07-12 17:12:23 +0200

    Predisposto attore di gestione del gioco che hanno il compito di collegarsi nel cluster

---
+++ client/src/main/scala/it/cwmp/client/model/PlayerActor.scala
@@ -0,0 +1,36 @@
+package it.cwmp.client.model
+
+import akka.actor.{Actor, AddressFromURIString}
+import akka.cluster.Cluster
+import it.cwmp.model.Participant
+
+object PlayerIncomingMessages {
+
+  case class StartGame(participantList: List[Participant])
+  case object EndGame
+}
+
+import it.cwmp.client.model.PlayerIncomingMessages._
+class PlayerActor extends Actor {
+
+  override def receive: Receive = lobbyBehaviour
+
+  private def backToLobby: Unit = context.become(lobbyBehaviour)
+  private def lobbyBehaviour: Receive = {
+    case StartGame(participants) =>
+      connectTo(participants)
+      enterGame
+  }
+
+  private def enterGame: Unit = context.become(inGameBehaviour)
+  private def inGameBehaviour: Receive = {
+    case EndGame => backToLobby
+  }
+
+  private def connectTo(participants: List[Participant]): Unit = {
+    if (participants.head.address == self.path.address.toString) {
+      val cluster = Cluster(context.system)
+      cluster.joinSeedNodes(participants.map(_.address).map(AddressFromURIString(_)))
+    }
+  }
+}
