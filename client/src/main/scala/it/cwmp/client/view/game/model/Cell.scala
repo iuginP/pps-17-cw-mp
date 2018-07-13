1dd843bac09ea56683a417c8b20e1d4abc2538e8
Davide Borficchia <davide.borficchia@studio.unibo.it>
2018-07-13 12:14:20 +0200

    aggiornata documentazione

--- client/src/main/scala/it/cwmp/client/view/game/model/Cell.scala
+++ client/src/main/scala/it/cwmp/client/view/game/model/Cell.scala
@@ -1,11 +1,10 @@
 package it.cwmp.client.view.game.model
 /**
+  * Classe che rappresenta una cella
   *
   * @author Davide Borficchia
   * @author Eugenio Pierfederici
-  */
-/**
-  * Classe che rappresenta una cella
+  *
   * @param center punto nel quale verrà disegnata la cella
   * @param size dimensione della cella
   */
--- client/src/main/scala/it/cwmp/client/view/game/model/Point.scala
+++ client/src/main/scala/it/cwmp/client/view/game/model/Point.scala
@@ -1,11 +1,10 @@
 package it.cwmp.client.view.game.model
 /**
+  * Classe che rappresenta una coppia di valori che identificano un punto
   *
   * @author Davide Borficchia
   * @author Eugenio Pierfederici
-  */
-/**
-  * Classe che rappresenta una coppia di valori che identificano un punto
+  *
   * @param x coordinata x del punto
   * @param y coordinata y del punto
   *
