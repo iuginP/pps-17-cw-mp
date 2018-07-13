a62b048c44810380e94b9f0a180c38e825feabab
iugin <eugenio.pierfederici@studio.unibo.it>
2018-07-13 10:42:19 +0200

    Predisposto hello world del cluster distribuito

--- client/src/main/scala/it/cwmp/client/model/PlayerActor.scala
+++ client/src/main/scala/it/cwmp/client/model/PlayerActor.scala
@@ -1,8 +1,12 @@
 package it.cwmp.client.model
 
-import akka.actor.{Actor, AddressFromURIString}
+import akka.actor.{Actor, ActorRef, AddressFromURIString}
 import akka.cluster.Cluster
+import akka.cluster.ddata.{DistributedData, Flag, FlagKey}
+import akka.cluster.ddata.Replicator.{Changed, Subscribe, Update, WriteAll}
 import it.cwmp.model.Participant
+
+import scala.concurrent.duration._
 
 object PlayerIncomingMessages {
 
@@ -20,12 +24,22 @@ object PlayerIncomingMessages {
 import it.cwmp.client.model.PlayerIncomingMessages._
 class PlayerActor extends Actor {
 
-  override def receive: Receive = lobbyBehaviour
+  val replicator: ActorRef = DistributedData(context.system).replicator
+  implicit val cluster: Cluster = Cluster(context.system)
+
+  val TestKey = FlagKey("contestTest")
+  replicator ! Subscribe(TestKey, self)
+
+  override def receive: Receive = lobbyBehaviour orElse {
+    case c @ Changed(TestKey) if c.get(TestKey).enabled =>
+      println("Receiving... Hello distributed! By " + self.path.address.toString)
+  }
 
   private def backToLobby: Unit = context.become(lobbyBehaviour)
   private def lobbyBehaviour: Receive = {
     case StartGame(participants) =>
       connectTo(participants)
+      sayHello()
       enterGame
   }
 
@@ -37,8 +51,12 @@ class PlayerActor extends Actor {
   private def connectTo(participants: List[Participant]): Unit = {
     // If it's the first of the list, then he creates the cluster.
     if (participants.head.address == self.path.address.toString) {
-      val cluster = Cluster(context.system)
       cluster.joinSeedNodes(participants.map(_.address).map(AddressFromURIString(_)))
     }
+  }
+
+  private def sayHello(): Unit = {
+    replicator ! Update(TestKey, Flag(), WriteAll(5.seconds))(_.switchOn)
+    println("Sending... Hello distributed! By " + self.path.address.toString)
   }
 }
