28f6c103be7eab12f1f999d79d22fc1f3d2f5143
Davide Borficchia <davide.borficchia@studio.unibo.it>
2018-07-13 12:07:45 +0200

    aggiornata documentazione

--- client/src/main/scala/it/cwmp/client/view/game/model/Cell.scala
+++ client/src/main/scala/it/cwmp/client/view/game/model/Cell.scala
@@ -1,3 +1,12 @@
 package it.cwmp.client.view.game.model
-
+/**
+  *
+  * @author Davide Borficchia
+  * @author Eugenio Pierfederici
+  */
+/**
+  * Classe che rappresenta una cella
+  * @param center punto nel quale verrà disegnata la cella
+  * @param size dimensione della cella
+  */
 case class Cell(center: Point, size: Int = 20)
--- client/src/main/scala/it/cwmp/client/view/game/model/Point.scala
+++ client/src/main/scala/it/cwmp/client/view/game/model/Point.scala
@@ -1,3 +1,13 @@
 package it.cwmp.client.view.game.model
-
+/**
+  *
+  * @author Davide Borficchia
+  * @author Eugenio Pierfederici
+  */
+/**
+  * Classe che rappresenta una coppia di valori che identificano un punto
+  * @param x coordinata x del punto
+  * @param y coordinata y del punto
+  *
+  */
 case class Point(x: Int, y: Int)
