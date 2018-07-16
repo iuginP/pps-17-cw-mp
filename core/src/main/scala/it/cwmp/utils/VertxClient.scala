e6af122b9ee90b67b334cdc806dcafbfa26bd061
iugin <eugenio.pierfederici@studio.unibo.it>
2018-07-16 17:19:13 +0200

    Aggiunto implicito nel VertxClient per aggiungere in automatico l'autenticazione

--- core/src/main/scala/it/cwmp/utils/VertxClient.scala
+++ core/src/main/scala/it/cwmp/utils/VertxClient.scala
@@ -1,6 +1,6 @@
 package it.cwmp.utils
 
-import io.vertx.scala.ext.web.client.{WebClient, WebClientOptions}
+import io.vertx.scala.ext.web.client.{HttpRequest, WebClient, WebClientOptions}
 
 trait VertxClient {
   this: VertxInstance =>
@@ -8,4 +8,21 @@ trait VertxClient {
   protected def clientOptions: WebClientOptions
 
   protected def client: WebClient = WebClient.create(vertx, clientOptions)
+}
+
+object VertxClient {
+
+  import io.netty.handler.codec.http.HttpHeaderNames
+  implicit class richHttpRequest[T](request: HttpRequest[T]) {
+
+    def addAuthentication(username: String, password: String) =
+      HttpUtils.buildBasicAuthentication(username, password)
+        .map(request.putHeader(HttpHeaderNames.AUTHORIZATION.toString, _))
+        .getOrElse(request)
+
+    def addAuthentication(token: String) =
+      HttpUtils.buildJwtAuthentication(token)
+        .map(request.putHeader(HttpHeaderNames.AUTHORIZATION.toString, _))
+        .getOrElse(request)
+  }
 }
