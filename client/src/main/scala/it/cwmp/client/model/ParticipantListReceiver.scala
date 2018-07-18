570ca194abb4f0fbfceb9f048e20b5b7373b8ab6
iugin <eugenio.pierfederici@studio.unibo.it>
2018-07-19 00:56:15 +0200

    Rifattorizzato room-receiver per usare le nuove strutture

--- client/src/main/scala/it/cwmp/client/model/ParticipantListReceiver.scala
+++ client/src/main/scala/it/cwmp/client/model/ParticipantListReceiver.scala
@@ -3,8 +3,8 @@ package it.cwmp.client.model
 import java.net.InetAddress
 
 import it.cwmp.model.{Address, Participant}
-import it.cwmp.roomreceiver.controller.RoomReceiverServiceVerticle
-import it.cwmp.services.wrapper.RoomReceiverApiWrapper
+import it.cwmp.services.roomreceiver.RoomReceiverServiceVerticle
+import it.cwmp.services.roomreceiver.ServerParameters._
 import it.cwmp.utils.{Utils, VertxInstance}
 
 import scala.concurrent.Future
@@ -16,6 +16,6 @@ trait ParticipantListReceiver extends VertxInstance {
     val verticle = RoomReceiverServiceVerticle(token, participants => onListReceived(participants))
     vertx.deployVerticleFuture(verticle)
       .map(_ => Address(s"http://${InetAddress.getLocalHost.getHostAddress}:${verticle.port}"
-        + RoomReceiverApiWrapper.API_RECEIVE_PARTICIPANTS_URL(token)))
+        + API_RECEIVE_PARTICIPANTS_URL(token)))
   }
 }
