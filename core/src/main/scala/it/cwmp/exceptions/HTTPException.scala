05d53c60238974690e0dc0f20fa90d617b079cf2
Davide Borficchia <davide.borficchia@studio.unibo.it>
2018-07-20 09:12:17 +0200

    implementata classe per gestire le HttpException

--- core/src/main/scala/it/cwmp/exceptions/HTTPException.scala
+++ core/src/main/scala/it/cwmp/exceptions/HTTPException.scala
@@ -1,9 +1,34 @@
 package it.cwmp.exceptions
 
 /**
-  * A class describing an Http Exception
-  *
-  * @param statusCode the HTTP code of error
-  * @param message    the error message
+  * This trait represents an http exception. It contains the status code and an optional error message.
   */
-sealed case class HTTPException(statusCode: Int, message: Option[String] = None) extends RuntimeException
+trait HttpException extends RuntimeException {
+  def statusCode: Int
+  def message: Option[String]
+}
+
+object HTTPException {
+  /**
+    * This constructor returns a new [[HTTPException]] with the provided status code
+    * @param statusCode The statuscode
+    * @return the HttpException
+    */
+  def apply(statusCode: Int): HttpException = HTTPExceptionImpl(statusCode, None)
+
+  /**
+    * This constructor returns a new [[HTTPException]] with the provided status code and message
+    * @param statusCode The statuscode
+    * @param message the error message
+    * @return the HttpException
+    */
+  def apply(statusCode: Int, message: String): HttpException = HTTPExceptionImpl(statusCode, Option(message))
+
+  /**
+    * A class describing an Http Exception
+    *
+    * @param statusCode the HTTP code of error
+    * @param message    the error message
+    */
+  sealed case class HTTPExceptionImpl(statusCode: Int, message: Option[String] = None) extends HttpException
+}
