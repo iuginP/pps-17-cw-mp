f959a084fc3d03f3b7e2d04b3edb0a4e3ab0af8b
Davide Borficchia <davide.borficchia@studio.unibo.it>
2018-07-21 16:18:34 +0200

    implementato struttura base per gestire i messaggi degli attori per modificare lo stato di gioco distribuito

--- client/src/main/scala/it/cwmp/client/model/DistributedState.scala
+++ client/src/main/scala/it/cwmp/client/model/DistributedState.scala
@@ -8,6 +8,20 @@ import akka.cluster.ddata._
 import it.cwmp.client.model.game.{Attack, Character, World}
 import it.cwmp.utils.Logging
 
+object DistributedStateMessages {
+
+  /**
+    * The message received when the user do something in the GUI
+    *
+    * @param world The world to update in the GUI
+    * @tparam Instant        Actual instance of the GameEngine
+    * @tparam WorldCharacter Actual cell in the world
+    * @tparam WorldAttack    Actual tentacle
+    */
+  case class UpdateWorld[Instant, WorldCharacter <: Character[_, _, _], WorldAttack <: Attack[_, _, _]](world: World[Instant, WorldCharacter, WorldAttack])
+
+}
+
 /**
   * Distributed representation of the world and of his behaviours.
   *
@@ -19,14 +33,14 @@ import it.cwmp.utils.Logging
 case class DistributedState[Instant, WorldCharacter <: Character[_, _, _], WorldAttack <: Attack[_, _, _]]
 (actor: Actor, replicator: Replicator, onWorldUpdate: World[Instant, WorldCharacter, WorldAttack] => Unit) extends Logging {
 
-  private val instantKey = GCounterKey("instant")
-  private val charactersKey = ORSetKey[WorldCharacter]("characters")
-  private val attacksKey = ORSetKey[WorldAttack]("attacks")
+  private val InstantKey = LWWRegisterKey[Instant]("instant")
+  private val CharactersKey = ORSetKey[WorldCharacter]("characters")
+  private val AttacksKey = ORSetKey[WorldAttack]("attacks")
 
-  def initState: Unit = {
-    replicator.self ! Subscribe(instantKey, actor.self)
-    replicator.self ! Subscribe(charactersKey, actor.self)
-    replicator.self ! Subscribe(attacksKey, actor.self)
+  def initState(): Unit = {
+    replicator.self ! Subscribe(InstantKey, actor.self)
+    replicator.self ! Subscribe(CharactersKey, actor.self)
+    replicator.self ! Subscribe(AttacksKey, actor.self)
   }
 
   /**
@@ -39,14 +53,30 @@ case class DistributedState[Instant, WorldCharacter <: Character[_, _, _], World
     *
     * @return
     */
-  private def userActionBehaviour: Receive = ???
+
+  import it.cwmp.client.model.DistributedStateMessages._
+
+  private def userActionBehaviour: Receive = {
+    // Called from the view/controller
+    case UpdateWorld(world: World[Instant, WorldCharacter, WorldAttack]) =>
+      updateWorld(world)
+
+  }
 
   /**
     * All the behaviours needed to manage the distribution of the state
     *
     * @return
     */
-  private def distributedActionBehaviour: Receive = ???
+  private def distributedActionBehaviour: Receive = {
+    // Called when notified of the distributed data change
+    case c@Changed(InstantKey) =>
+      log.debug("UpdateGUI from DISTRIBUTED because instantKey was changed")
+    case c@Changed(CharactersKey) =>
+      log.debug("UpdateGUI from DISTRIBUTED because charactersKey was changed")
+    case c@Changed(AttacksKey) =>
+      log.debug("UpdateGUI from DISTRIBUTED because attacksKey was changed")
+  }
 
   /**
     * Used to map the distributed world to the World instance.
@@ -61,6 +91,9 @@ case class DistributedState[Instant, WorldCharacter <: Character[_, _, _], World
     * @param world the new version of the world
     */
   private def updateWorld(world: World[Instant, WorldCharacter, WorldAttack]): Unit = {
-//    replicator.self ! Update(instantKey, GCounter.empty, WriteLocal)(_ + 1) TODO example
+    log.debug("UpdateGUI from user")
+    replicator.self ! Update(InstantKey, LWWRegister[Instant](world.instant), WriteLocal)(_.withValue(world.instant)) //TODO inviare il nuovo stato aggiornato
+    replicator.self ! Update(CharactersKey, ORSet.empty[WorldCharacter], WriteLocal)(_) //TODO inviare il nuovo stato aggiornato
+    replicator.self ! Update(AttacksKey, ORSet.empty[WorldAttack], WriteLocal)(_) //TODO inviare il nuovo stato aggiornato
   }
 }
