c49e256973d8e2e7afb4d1a9dfcbef3cc6091cad
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2018-07-24 20:59:58 +0200

    - Added doc - Reformatted code, solved warnings

--- services/room-receiver/src/test/scala/it/cwmp/services/testing/roomreceiver/RoomReceiverTesting.scala
+++ services/room-receiver/src/test/scala/it/cwmp/services/testing/roomreceiver/RoomReceiverTesting.scala
@@ -1,19 +1,23 @@
 package it.cwmp.services.testing.roomreceiver
 
 import it.cwmp.model.Participant
+import it.cwmp.services.testing.roomreceiver.RoomReceiverTesting.TOKEN_LENGTH
 import it.cwmp.testing.VertxTest
 import it.cwmp.utils.Utils
 import org.scalatest.{BeforeAndAfterEach, Matchers}
 
 import scala.concurrent.Promise
 
+/**
+  * An abstract base class for testing Room receiver service
+  */
 abstract class RoomReceiverTesting extends VertxTest with Matchers with BeforeAndAfterEach {
 
   protected var port: Int
 
-  protected val rightToken: String = Utils.randomString(20)
+  protected val rightToken: String = Utils.randomString(TOKEN_LENGTH)
 
-  protected val wrongToken: String = Utils.randomString(20)
+  protected val wrongToken: String = Utils.randomString(TOKEN_LENGTH)
 
   protected val participants: List[Participant] = List(
     Participant("Nome", "Indirizzo"),
@@ -27,4 +31,11 @@ abstract class RoomReceiverTesting extends VertxTest with Matchers with BeforeAn
     participantsPromise = Promise()
   }
 
+}
+
+/**
+  * Companion object
+  */
+object RoomReceiverTesting {
+  private val TOKEN_LENGTH = 20
 }
--- services/room-receiver/src/test/scala/it/cwmp/services/testing/roomreceiver/RoomReceiverWebTesting.scala
+++ services/room-receiver/src/test/scala/it/cwmp/services/testing/roomreceiver/RoomReceiverWebTesting.scala
@@ -3,12 +3,15 @@ package it.cwmp.services.testing.roomreceiver
 import it.cwmp.services.roomreceiver.RoomReceiverServiceVerticle
 import it.cwmp.testing.VerticleBeforeAndAfterEach
 
+/**
+  * An abstract base class for testing the room receiver web service
+  */
 abstract class RoomReceiverWebTesting extends RoomReceiverTesting with VerticleBeforeAndAfterEach {
 
   override var port: Int = _
 
-  override protected val verticlesBeforeEach: List[RoomReceiverServiceVerticle] = List(
-    RoomReceiverServiceVerticle(rightToken, s => participantsPromise.success(s)))
+  override protected val verticlesBeforeEach: List[RoomReceiverServiceVerticle] =
+    List(RoomReceiverServiceVerticle(rightToken, s => participantsPromise.success(s)))
 
   override def beforeEach(): Unit = {
     super.beforeEach()
