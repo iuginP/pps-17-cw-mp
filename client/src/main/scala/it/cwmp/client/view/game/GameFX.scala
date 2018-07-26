d4d9c7fe9538293031b70b3623a36b80698f01c2
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2018-07-26 11:43:47 +0200

    Added an object in GameFX that will manage user made events on GUI

--- client/src/main/scala/it/cwmp/client/view/game/GameFX.scala
+++ client/src/main/scala/it/cwmp/client/view/game/GameFX.scala
@@ -1,18 +1,22 @@
 package it.cwmp.client.view.game
 
-import it.cwmp.client.model.game.impl.CellWorld
+import it.cwmp.client.model.game.impl.{CellWorld, Point}
 import it.cwmp.client.view.game.model.CellView._
 import it.cwmp.client.view.game.model.TentacleView
 import javafx.application.Platform
 import javafx.embed.swing.JFXPanel
 import javafx.scene.canvas.{Canvas, GraphicsContext}
+import javafx.scene.input.MouseEvent
 import javafx.scene.{Group, Scene}
 import javafx.stage.Stage
+
+import scala.language.implicitConversions
 
 /**
   * This class shows the Game GUI
   *
   * @author Davide Borficchia
+  * @author contributor Enrico Siboni
   */
 case class GameFX() extends CellWorldObjectDrawer {
 
@@ -32,6 +36,8 @@ case class GameFX() extends CellWorldObjectDrawer {
       stage = new Stage
       root = new Group
       canvas = new Canvas(size, size)
+
+      UserEventHandler.initializeEventHandlers(root)
 
       stage.setTitle(title)
       root.getChildren.add(canvas)
@@ -71,4 +77,53 @@ case class GameFX() extends CellWorldObjectDrawer {
       root.getChildren.add(drawInstant(world.instant))
     })
   }
+
+  /**
+    * An object that wraps logic behind user events un GUI
+    *
+    * @author Enrico Siboni
+    */
+  private object UserEventHandler {
+
+    private var mouseIsDragging = false
+
+    private var startDragPoint: Point = _
+
+    /**
+      * A method to initialize event handlers for GUI user actions
+      *
+      * @param viewGroup the viewGroup on which to listen for events
+      */
+    def initializeEventHandlers(viewGroup: Group): Unit = {
+
+      viewGroup.addEventHandler(MouseEvent.DRAG_DETECTED, (event: MouseEvent) => {
+        mouseIsDragging = true
+        startDragPoint = event
+      })
+      viewGroup.addEventHandler(MouseEvent.MOUSE_RELEASED, (event: MouseEvent) =>
+        if (mouseIsDragging) {
+          val stopDragPoint: Point = event
+          // TODO: send a message to GameViewActor
+        })
+
+      viewGroup.addEventHandler(MouseEvent.MOUSE_CLICKED, (event: MouseEvent) =>
+        if (mouseIsDragging) {
+          mouseIsDragging = false
+        } else {
+          val clickedPoint: Point = event
+          // TODO: send a message to GameViewActor
+        }
+      )
+    }
+
+    /**
+      * An implicit conversion from mouse event to the point where event was generated
+      *
+      * @param event the event to convert
+      * @return the Point where event was generated
+      */
+    implicit def eventToPoint(event: MouseEvent): Point =
+      Point(event.getX.toInt, event.getY.toInt)
+  }
+
 }
