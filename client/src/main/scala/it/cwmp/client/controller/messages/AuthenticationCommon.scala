a2c50ab97bc9c9c7ac775451fd445017e7f96ce1
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2018-07-29 23:20:07 +0200

    Added type hierarchy to Authentication Common messages

--- client/src/main/scala/it/cwmp/client/controller/messages/AuthenticationCommon.scala
+++ client/src/main/scala/it/cwmp/client/controller/messages/AuthenticationCommon.scala
@@ -6,12 +6,17 @@ package it.cwmp.client.controller.messages
 object AuthenticationRequests {
 
   /**
+    * A trait representing an authentication request
+    */
+  sealed trait AuthenticationRequest
+
+  /**
     * Request to log-in
     *
     * @param username the player username
     * @param password the player password
     */
-  sealed case class LogIn(username: String, password: String)
+  sealed case class LogIn(username: String, password: String) extends AuthenticationRequest
 
   /**
     * Request to sign-up
@@ -19,14 +24,14 @@ object AuthenticationRequests {
     * @param username the player username
     * @param password the player password
     */
-  sealed case class SignUp(username: String, password: String)
+  sealed case class SignUp(username: String, password: String) extends AuthenticationRequest
 
   /**
     * Request to log-out
     *
     * @param username the player username that's signing-out
     */
-  sealed case class LogOut(username: String)
+  sealed case class LogOut(username: String) extends AuthenticationRequest
 
 }
 
@@ -36,31 +41,46 @@ object AuthenticationRequests {
 object AuthenticationResponses {
 
   /**
+    * A trait representing an authentication response
+    */
+  sealed trait AuthenticationResponse
+
+  /**
+    * A trait representing LogInResponses
+    */
+  sealed trait LogInResponse extends AuthenticationResponse
+
+  /**
+    * A trait representing SignUpResponses
+    */
+  sealed trait SignUpResponse extends AuthenticationResponse
+
+  /**
     * Log-in succeeded
     *
     * @param token the user identifying token
     */
-  sealed case class LogInSuccess(token: String)
+  sealed case class LogInSuccess(token: String) extends LogInResponse
 
   /**
     * Log-in failure
     *
     * @param errorMessage optionally an error message
     */
-  sealed case class LogInFailure(errorMessage: Option[String])
+  sealed case class LogInFailure(errorMessage: Option[String]) extends LogInResponse
 
   /**
     * Sign-up succeeded
     *
     * @param token the user identifying token
     */
-  sealed case class SignUpSuccess(token: String)
+  sealed case class SignUpSuccess(token: String) extends SignUpResponse
 
   /**
     * Sign-up failure
     *
     * @param errorMessage optionally an error message
     */
-  sealed case class SignUpFailure(errorMessage: Option[String])
+  sealed case class SignUpFailure(errorMessage: Option[String]) extends SignUpResponse
 
 }
