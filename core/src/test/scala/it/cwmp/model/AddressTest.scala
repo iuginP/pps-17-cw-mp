8f05fca1922cb8a1219a54a4837ff43c970ecca3
Elia Di Pasquale <elia.dipasquale@studio.unibo.it>
2018-08-04 18:19:05 +0200

    Started with address test.

---
+++ core/src/test/scala/it/cwmp/model/AddressTest.scala
@@ -0,0 +1,17 @@
+package it.cwmp.model
+
+import org.scalatest.FunSpec
+
+/**
+  * Test class for model class Address
+  *
+  * @author Elia Di Pasquale
+  */
+class AddressTest extends FunSpec {
+
+  private val address = Address("general_address")
+
+  describe("An address") {
+
+  }
+}
