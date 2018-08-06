38156a0744561e3bdea2ea288a8652e236567736
iugin <eugenio.pierfederici@studio.unibo.it>
2018-08-06 15:54:46 +0200

    Creato fake AuthenticationWrapper per i test

--- client/src/test/scala/it/cwmp/client/controller/ApiClientActorTest.scala
+++ client/src/test/scala/it/cwmp/client/controller/ApiClientActorTest.scala
@@ -3,8 +3,12 @@ package it.cwmp.client.controller
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import it.cwmp.client.controller.messages.{AuthenticationRequests, AuthenticationResponses}
+import it.cwmp.model.{Address, Room}
+import it.cwmp.services.wrapper.{FakeAuthenticationApiWrapper, RoomsApiWrapper}
 import it.cwmp.utils.Utils
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import scala.concurrent.Future
 
 class ApiClientActorTest extends TestKit(ActorSystem("MySpec")) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
@@ -15,24 +19,44 @@ class ApiClientActorTest extends TestKit(ActorSystem("MySpec")) with ImplicitSen
     TestKit.shutdownActorSystem(system)
   }
 
-  private val actor: ActorRef = system.actorOf(Props(ApiClientActor()), ApiClientActor.getClass.getName)
-
   private val USERNAME_LENGTH: Int = 10
   private val PASSWORD_LENGTH: Int = 10
 
   private def nextUsername: String = Utils.randomString(USERNAME_LENGTH)
+
   private def nextPassword: String = Utils.randomString(PASSWORD_LENGTH)
+
+  private val actor: ActorRef = system.actorOf(Props(ApiClientActor(
+    FakeAuthenticationApiWrapper(),
+    new RoomsApiWrapper {
+      override def createRoom(roomName: String, playersNumber: Int)(implicit userToken: String): Future[String] = ???
+
+      override def enterRoom(roomID: String, userAddress: Address, notificationAddress: Address)(implicit userToken: String): Future[Unit] = ???
+
+      override def roomInfo(roomID: String)(implicit userToken: String): Future[Room] = ???
+
+      override def exitRoom(roomID: String)(implicit userToken: String): Future[Unit] = ???
+
+      override def listPublicRooms()(implicit userToken: String): Future[Seq[Room]] = ???
+
+      override def enterPublicRoom(playersNumber: Int, userAddress: Address, notificationAddress: Address)(implicit userToken: String): Future[Unit] = ???
+
+      override def publicRoomInfo(playersNumber: Int)(implicit userToken: String): Future[Room] = ???
+
+      override def exitPublicRoom(playersNumber: Int)(implicit userToken: String): Future[Unit] = ???
+    }
+  )), ApiClientActor.getClass.getName)
 
   "API authentication actor" must {
 
     "return the token when successful signUp" in {
       actor ! AuthenticationRequests.SignUp(nextUsername, nextPassword)
-      expectMsg(AuthenticationResponses.SignUpSuccess(_))
+      expectMsgType[AuthenticationResponses.SignUpSuccess]
     }
 
     "return the token when successful login" in {
       actor ! AuthenticationRequests.LogIn(nextUsername, nextPassword)
-      expectMsg(AuthenticationResponses.LogInSuccess(_))
+      expectMsgType[AuthenticationResponses.LogInSuccess]
     }
 
   }
---
+++ core/src/test/scala/it/cwmp/services/wrapper/FakeAuthenticationApiWrapper.scala
@@ -0,0 +1,51 @@
+package it.cwmp.services.wrapper
+
+import io.netty.handler.codec.http.HttpResponseStatus.{BAD_REQUEST, UNAUTHORIZED}
+import it.cwmp.exceptions.HTTPException
+import it.cwmp.model.{Participant, User}
+import it.cwmp.utils.HttpUtils
+import it.cwmp.utils.Utils.httpStatusNameToCode
+
+import scala.concurrent.Future
+
+case class FakeAuthenticationApiWrapper() extends AuthenticationApiWrapper {
+
+  private val participantAddress = "http://127.0.1.1:8668/api/client/pFU9qOCU3kmYqwk1qqkl/room/participants"
+
+  private var participantPasswords: Map[String, String] = Map()
+  private var participants: Map[String, Participant] = Map()
+
+  override def signUp(usernameCheck: String, passwordCheck: String): Future[String] =
+    (for (
+      username <- Option(usernameCheck);
+      password <- Option(passwordCheck);
+      token = tokenFromUsername(username)
+    ) yield {
+      if (participants.contains(token)) {
+        Future.failed(HTTPException(BAD_REQUEST))
+      } else {
+        participants = participants + (token -> Participant(username, participantAddress))
+        participantPasswords = participantPasswords + (s"$username:$password" -> token)
+        Future.successful(token)
+      }
+    }).getOrElse(Future.failed(HTTPException(BAD_REQUEST)))
+
+  override def login(usernameCheck: String, passwordCheck: String): Future[String] = (for (
+    username <- Option(usernameCheck);
+    password <- Option(passwordCheck)
+  ) yield {
+    participantPasswords.get(s"$username:$password")
+      .map(Future.successful)
+      .getOrElse(Future.failed(HTTPException(UNAUTHORIZED)))
+  }).getOrElse(Future.failed(HTTPException(BAD_REQUEST)))
+
+  override def validate(authorizationHeader: String): Future[User] = HttpUtils.readJwtAuthentication(authorizationHeader) match {
+    case Some(token) => participants.get(token) match {
+      case Some(participant) => Future.successful(participant)
+      case _ => Future.failed(HTTPException(UNAUTHORIZED))
+    }
+    case _ => Future.failed(HTTPException(UNAUTHORIZED))
+  }
+
+  private def tokenFromUsername(username: String): String = "TOKEN_" + username
+}
