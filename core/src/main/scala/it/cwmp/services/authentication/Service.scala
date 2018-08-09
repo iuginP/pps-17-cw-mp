df6fe6705612e78c17ee51e89ac043174d191f0d
iugin <eugenio.pierfederici@studio.unibo.it>
2018-08-09 14:06:03 +0200

    Documentato gli oggetti service

--- core/src/main/scala/it/cwmp/services/authentication/Service.scala
+++ core/src/main/scala/it/cwmp/services/authentication/Service.scala
@@ -1,5 +1,12 @@
 package it.cwmp.services.authentication
 
+/**
+  * This object contains every parameter and configuration
+  * needed in order to execute the authentication service.
+  */
 object Service {
+  /**
+    * The service identifier in the discovery service.
+    */
   val DISCOVERY_NAME = "authentication_service"
 }
--- core/src/main/scala/it/cwmp/services/rooms/Service.scala
+++ core/src/main/scala/it/cwmp/services/rooms/Service.scala
@@ -1,5 +1,12 @@
 package it.cwmp.services.rooms
 
+/**
+  * This object contains every parameter and configuration
+  * needed in order to execute the rooms service.
+  */
 object Service {
+  /**
+    * The service identifier in the discovery service.
+    */
   val DISCOVERY_NAME = "rooms_service"
 }
