6841d95509f11dcc15a56b1ac3d295eb1b9c6070
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2018-08-09 21:40:58 +0200

    Transformed OneAddressInput to case class

--- core/src/main/scala/it/cwmp/view/OneAddressInput.scala
+++ core/src/main/scala/it/cwmp/view/OneAddressInput.scala
@@ -9,19 +9,9 @@ import it.cwmp.view.AbstractAddressInput.{defaultPort, localIP}
   * @param message       the message to show the user
   * @param onResultReady the action on result ready
   */
-class OneAddressInput(viewTitle: String, message: String, onResultReady: ((String, String)) => Unit)
-                     (defaultIP: String = localIP, defaultPort: String = defaultPort)
+case class OneAddressInput(viewTitle: String, message: String, onResultReady: ((String, String)) => Unit)
+                          (defaultIP: String = localIP, defaultPort: String = defaultPort)
   extends AbstractAddressInput[(String, String)](viewTitle, message, onResultReady)(defaultIP, defaultPort) {
 
   override protected def getResult: (String, String) = (addressIpField.getText, addressPortField.getText)
-}
-
-/**
-  * Companion object
-  */
-object OneAddressInput {
-
-  def apply(viewTitle: String, message: String, onResultReady: ((String, String)) => Unit)
-           (defaultIP: String = localIP, defaultPort: String = defaultPort): OneAddressInput =
-    new OneAddressInput(viewTitle, message, onResultReady)(defaultIP, defaultPort)
 }
