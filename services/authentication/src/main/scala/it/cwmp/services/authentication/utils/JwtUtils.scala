c3b87ea83d05511e704c07d63ebb976558fde66e
Davide <davide.borficchia@studio.unibo.it>
2018-08-20 10:36:53 +0200

    Aggiunta documentazione

--- services/authentication/src/main/scala/it/cwmp/services/authentication/utils/JwtUtils.scala
+++ services/authentication/src/main/scala/it/cwmp/services/authentication/utils/JwtUtils.scala
@@ -5,7 +5,10 @@ import pdi.jwt.{Jwt, JwtAlgorithm, JwtClaim}
 
 import scala.util.{Failure, Success}
 
-// TODO: add doc to all class methods
+/**
+  * Questa classe serve per effettuare le funzioni basi con i JWT tokens
+  * @author Davide Borficchia
+  */
 object JwtUtils {
 
   val USERNAME_FIELD_NAME: String = "username"
@@ -13,10 +16,20 @@ object JwtUtils {
   private val secretKey = "secretKey"
   private val algorithm = JwtAlgorithm.HS256
 
+  /**
+    * Crea un token a partire da un claim
+    * @param claim è il testo che verrà inserito dentro il token
+    * @return il token da utilizzare nelle richieste
+    */
   def encodeToken(claim: JwtClaim): Option[String] =
     if (claim != null) Some(Jwt.encode(claim, secretKey, algorithm))
     else None
 
+  /**
+    * Dato un token lo decodifica
+    * @param token il token da decodificare
+    * @return il contenuto in chiaro del token
+    */
   def decodeToken(token: String): Option[JwtClaim] = {
     Jwt.decodeRawAll(token, secretKey, Seq(algorithm)) match {
       case Success((_, b, _)) =>
@@ -25,15 +38,30 @@ object JwtUtils {
     }
   }
 
+  /**
+    * Controlla la validità di un token
+    * @param token token da validare
+    * @return true se il token è valido, false in caso contrario
+    */
   def validateToken(token: String): Boolean = {
     Jwt.isValid(token, secretKey, Seq(algorithm))
   }
 
+  /**
+    * Crea il token contenente l'username
+    * @param username username da inserire nel token
+    * @return il token da utilizzare nelle richieste
+    */
   def encodeUsernameToken(username: String): Option[String] = {
     if (username == null) None
     else encodeToken(JwtClaim(new JsonObject().put(USERNAME_FIELD_NAME, username).encode()))
   }
 
+  /**
+    * Dato un token lo decodifica e risale all'username
+    * @param token token da decodificare
+    * @return username dell'utente
+    */
   def decodeUsernameToken(token: String): Option[String] = {
     decodeToken(token).map(decoded => new JsonObject(decoded.content).getString(USERNAME_FIELD_NAME))
   }
